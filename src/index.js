c2a6a0f26639453d76e434f716f1877d341a88cf
Martín Alejandro Barros <martinbarros86@gmail.com>
2020-06-20 18:36:26 -0300

    Converto to PWA: service worker to register

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import 'antd/dist/antd.css';
-import thunk from 'redux-thunk'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import "antd/dist/antd.css";
+import thunk from "redux-thunk";
 import { getFetchedProducts } from "./actions";
 import { createLogger } from "redux-logger";
 
-const middleware = [ thunk ]
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
+const middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
 }
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -21,9 +21,9 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   composeEnhancer(applyMiddleware(...middleware))
-)
+);
 
-store.dispatch(getFetchedProducts())
+store.dispatch(getFetchedProducts());
 
 ReactDOM.render(
   <Provider store={store}>
@@ -31,10 +31,10 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
